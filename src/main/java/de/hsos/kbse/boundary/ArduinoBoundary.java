c28f80689b371c096413588b1df3047cbfb8e765
Jannik Bergmann <jannik.bergmann@hs-osnabrueck.de>
2020-07-20 11:01:31 +0200

    added rough design layout

--- src/main/java/de/hsos/kbse/boundary/ArduinoBoundary.java
+++ src/main/java/de/hsos/kbse/boundary/ArduinoBoundary.java
@@ -9,9 +9,18 @@ import de.hsos.kbse.controller.ArduinoRepoImpl;
 import de.hsos.kbse.entities.Arduino;
 import de.hsos.kbse.entities.SensorData;
 import java.io.Serializable;
+import javafx.scene.chart.PieChart;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.PieChartModel;
 
 /**
  *
@@ -19,11 +28,89 @@ import javax.inject.Named;
  */
 @ConversationScoped
 @Named(value = "arduino")
-public class ArduinoBoundary implements Serializable{
+@Getter
+@Setter
+public class ArduinoBoundary implements Serializable {
 
     @Inject
     ArduinoRepoImpl arduinoRepo;
-    
+
+    private PieChartModel waterlevel;
+    private PieChartModel soilhumidity;
+
+    private BarChartModel barModel;
+
+    @PostConstruct
+    public void init() {
+        //createWaterLevel();
+        createBarModel();
+    }
+
+    private void createPieModels() {
+        createWaterLevel();
+    }
+
+    private void createBarModel() {
+        barModel = initBarModel();
+
+        barModel.setTitle("Füllstände");
+        barModel.setLegendPosition("ne");
+
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        //xAxis.setLabel("Gender");
+
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Prozent");
+        yAxis.setMin(0);
+        yAxis.setMax(100);
+    }
+
+    private BarChartModel initBarModel() {
+        BarChartModel model = new BarChartModel();
+
+        ChartSeries waterlevel = new ChartSeries();
+        waterlevel.setLabel("Wasserfüllstand");
+        waterlevel.set("", 30);
+        
+
+        ChartSeries soilhumidity = new ChartSeries();
+        soilhumidity.setLabel("Bodenfeuchtigkeit");
+        soilhumidity.set("", 52);
+        
+        ChartSeries airhumidity = new ChartSeries();
+        airhumidity.setLabel("Bodenfeuchtigkeit");
+        airhumidity.set("", 72);
+        
+        ChartSeries lightintensity = new ChartSeries();
+        lightintensity.setLabel("Bodenfeuchtigkeit");
+        lightintensity.set("", 92);
+
+        model.addSeries(waterlevel);
+        model.addSeries(soilhumidity);
+        model.addSeries(airhumidity);
+        model.addSeries(lightintensity);
+        
+
+        return model;
+    }
+
+    private void createSoilHumidity() {
+        soilhumidity = new PieChartModel();
+
+    }
+
+    private void createWaterLevel() {
+        waterlevel = new PieChartModel();
+
+        waterlevel.set("Tanke gefüllt", 50);
+        waterlevel.set("Tank leer", 10);
+
+        waterlevel.setTitle("Wasserfüllstand");
+        waterlevel.setShowDataLabels(false);
+        //pieModel1.setLegendPosition("w");
+        waterlevel.setShadow(false);
+    }
+
     public void onClickDebug() {
         Arduino arduino = new Arduino();
         SensorData sensorData = new SensorData();
@@ -31,13 +118,12 @@ public class ArduinoBoundary implements Serializable{
         sensorData.setLightintensity(20);
         sensorData.setSoilhumidity(30);
         sensorData.setWaterlevel(40);
-        
+
         arduino.setSensorData(sensorData);
         arduino.setIpAddress("1111");
         System.out.println(arduino.toString());
         arduinoRepo.newArduino(arduino);
-        
-        
+
         System.out.println("Ich bin eine DebugNachricht (:");
     }
 }
